ca6f3393c53335e51f60aa6842c63f298d8c7e30
AyrexSigma <ayrex.ytz@gmail.com>
2025-06-13 12:26:46 +0300

    git create models.py

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def published_recently(self):
+        return (timezone.now() - self.published_date).days < 7
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author_name = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Comment by {self.author_name} on {self.post.title}"
